a6b0e928d7aa1e58262d95a25fcc2d29f01b0b11
RISHABH <gguptarishabh82@gmail.com>
2022-03-09 15:01:38 +0530

    Calling

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisonProblemUsingOops_Day_08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("... Welcome to Line Comparison Computation ... ");
+            LineComparison Linecom = new LineComparison();
+            Linecom.length_line();
+           
+            Console.ReadKey();
+        }
+    }
+}
